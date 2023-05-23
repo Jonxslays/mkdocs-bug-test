5f2fea87aad33fa8d7579a470af6fe540c696865
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2023-05-23 04:35:11 -0700

    Initial commit

---
+++ modules/main.py
@@ -0,0 +1,12 @@
+import secondary
+
+class Main:
+    """The main class."""
+
+    def mutate_secondary(self, _secondary: secondary.Secondary) -> None:
+        """Does something with secondary.
+
+        Args:
+            _secondary: The secondary instance.
+        """
+        ...
---
+++ modules/secondary.py
@@ -0,0 +1,3 @@
+class Secondary:
+    """Some secondary class."""
+    ...
